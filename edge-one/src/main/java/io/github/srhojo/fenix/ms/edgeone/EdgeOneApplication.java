fc588399b56e4d5ffca8629bd04d1b18f0863dd0
Javier lacalle <javihojo@gmail.com>
2020-11-21 10:20:06 +0100

    initial commit

---
+++ edge-one/src/main/java/io/github/srhojo/fenix/ms/edgeone/EdgeOneApplication.java
@@ -0,0 +1,13 @@
+package io.github.srhojo.fenix.ms.edgeone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgeOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgeOneApplication.class, args);
+	}
+
+}
---
+++ edge-one/src/test/java/io/github/srhojo/fenix/ms/edgeone/EdgeOneApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.srhojo.fenix.ms.edgeone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgeOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
